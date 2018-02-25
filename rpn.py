5fecaa05ff0f6bbac3fccc543cbc29d558a0df1f
Jordan Lozinski <dogowner777@gmail.com>
2018-02-24 20:00:32 -0500

    Added: percent arithmetic, previous answer symbol, summation operator, integer division, and exponent operator.

--- rpn.py
+++ rpn.py
@@ -1,33 +1,78 @@
 #!/usr/bin/env python3
 
 import operator
+import logging
+import sys
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+sh = logging.StreamHandler(sys.stdout)
+logger.addHandler(sh)
 
 operators = {
     '+': operator.add,
     '-': operator.sub,
     '*': operator.mul,
     '/': operator.truediv,
+    '^': operator.pow,
+    '.': operator.floordiv,
 }
 
+ans = ""
+
+def num(s):
+    try:
+        return int(s)
+    except ValueError:
+        return float(s)
+
+def percent(base, perc, order): #order is True if base comes before percent
+    perc = num(perc[0:-1])
+    perc = perc/100
+    base = num(base)
+    arg1 = base
+    arg2 = base*perc
+    return (arg1, arg2) if order else (arg2, arg1)
+
 def calculate(myarg):
+    global ans
     stack = list()
     for token in myarg.split():
-        try:
-            token = int(token)
-            stack.append(token)
-        except ValueError:
+        if token == ":ans":
+            if ans == "":
+                raise Exception("Cannot use answer without previous calculation")
+            stack.append(ans)
+        elif token not in operators.keys():
+            if token == "s":
+                count = 0
+                for c in range(0, len(stack)):
+                    b = stack.pop()
+                    count += num(b)
+                stack.append(count)
+            else:
+                stack.append(token)
+        else:
             function = operators[token]
             arg2 = stack.pop()
             arg1 = stack.pop()
-            result = function(arg1, arg2)
+            if arg1[-1] == "%":
+                arg1, arg2 = percent(arg2, arg1, False)
+            elif arg2[-1] == "%":
+                arg1, arg2 = percent(arg1, arg2, True)
+            else:
+                arg1 = num(arg1)
+                arg2 = num(arg2)
+            result = str(function(arg1, arg2)) #We want just strings on the stack. Keeps 
             stack.append(result)
-        print(stack)
+    logger.debug(stack)
     if len(stack) != 1:
         raise TypeError("Too many parameters")
-    return stack.pop()
+    ans = stack.pop()
+    return num(ans)
 
 def main():
+    global ans
+    ans = ""
     while True:
         result = calculate(input("rpn calc> "))
         print("Result: ", result)
--- test_rpn.py
+++ test_rpn.py
@@ -1,6 +1,10 @@
 import unittest
-
 import rpn
+
+class TestNoAns(unittest.TestCase): #Needed to isolate this test case
+    def test_no_ans(self):
+        with self.assertRaises(Exception):
+            result = rpn.calculate(":ans 2 +")
 
 class TestBasics(unittest.TestCase):
     def test_add(self):
@@ -15,3 +19,30 @@ class TestBasics(unittest.TestCase):
     def test_divide(self):
         result = rpn.calculate("6 3 /")
         self.assertEqual(2, result)
+    def test_toomany(self):
+        with self.assertRaises(TypeError):
+            result = rpn.calculate("1 2 3 +")
+    def test_percent(self):
+        res = rpn.calculate("72 5% +")
+        self.assertEqual(75.6, res)
+        res2 = rpn.calculate("72 20% - 5% +")
+        self.assertAlmostEqual(60.48, res2) #Different func to avoid floating point error
+        res3 = rpn.calculate("20% 10 -")
+        self.assertEqual(-8, res3)
+    def test_chained(self):
+        res = rpn.calculate("6 3 / 5 +")
+        self.assertEqual(7, res)
+    def test_ans(self):
+        rpn.calculate("6 3 +")
+        res = rpn.calculate(":ans 2 +")
+        self.assertEqual(11, res)
+    def test_sum(self):
+        res = rpn.calculate("5 3 2 1 10 -2 s")
+        self.assertEqual(19, res)
+
+def suite():
+    suite = unittest.TestSuite();
+    suite.addTest(TestNoAns())
+    suite.addTest(TestBasics())
+    return suite
+    
